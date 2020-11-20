4f16f85d6ecdda32605ad45598a3be72aabfb6f1
christoph <christoph@lafkon.net>
2020-11-20 15:51:04 +0100

    copy newer version (from freeze/faece245)
    - keep timestamp if file is up-to-date (+ocd formatting)
    - allow one.remote as input (was: only directories)
    - [ "$CHECKSUMLOKAL" != "$CHECKSUMREMOTE" ] (fix)
    - if $REMOTE is newer than $LOKAL
      create checksum before replacing
      -> keep lokal version if identical
    - support directory as target

--- utils/lokalize.sh
+++ utils/lokalize.sh
@@ -4,41 +4,40 @@
 # TODO: -f => force download
 #       check for README/LICENSE AT REMOTE LOCATION
 # ====================================================================== #
-  SRCDIR=`echo $* | grep "/" | head -n 1`
+  SRCPTH=`echo $* | sed 's/ /\n/g' | head -n 1`
 # ---------------------------------------------------------------------- #
-  if [ ! -d $SRCDIR ]
-   then echo "----"; echo "$SRCDIR NOT A DIRECTORY."
-      exit 0;
-  elif [ `echo "$SRCDIR" | wc -c` -lt 2 ]
-   then echo "----"; echo "CHECK/DOWNLOAD ALL SOURCES."
-        SRCDIR="."
-   if [ `find $SRCDIR -name "*.remote" | #
-         wc -l` -gt 0 ]; then
-        N=`cat \`find $SRCDIR -name "*.remote"\` | #
-                 grep "^[ \t]*https\?:" | wc -l`
-        echo -e "THIS MEANS CHECKING $N FILES \
-                 AND WILL TAKE SOME TIME.\n" | tr -s ' '
-        read -p "SHOULD WE DO IT? [y/n] " ANSWER
-        if [ X$ANSWER != Xy ] ; then echo "BYE."; exit 1;
-                                else echo; fi
-    else echo "NOTHING TO DO.";exit 0;
-   fi
+  if [ -f "$SRCPTH" ]
+  then ALLSRC="$SRCPTH"
+  elif [ -d "$SRCPTH" ]
+  then ALLSRC=`find $SRCPTH -name "*.remote"`
+  else echo "NO INPUT PROVIDED"
+       echo "----"; echo "CHECK/DOWNLOAD ALL SOURCES."
+       SRCPTH="../_"
+       ALLSRC=`find $SRCPTH -name "*.remote"`
   fi
+# --
+  if [ "$ALLSRC" == "" ];then echo "NOTHING TO DO.";exit 0;fi
+# --
+  N=`cat $ALLSRC | grep "^[ \t]*https\?:" | wc -l`
+  echo -e "THIS MEANS CHECKING $N FILES \
+           AND WILL TAKE SOME TIME.\n" | tr -s ' '
+  read -p "SHOULD WE DO IT? [y/n] " ANSWER
+  if [ "$ANSWER" != y ];then echo "BYE."; exit 1;
+                        else echo; fi
 # ---------------------------------------------------------------------- #
   echo -e "THE FOLLOWING PROCESS WILL DOWNLOAD FILES
   FROM DIFFERENT SOURCES WITH DIFFERENT COPYRIGHTS.
   IF NOT STATED OTHERWISE ALL RIGHTS RESERVED TO THE AUTHORS." | #
   sed 's/^[ ]*//'
   read -p "I KNOW WHAT I'M DOING? [y/n] " ANSWER
-  if [ X$ANSWER != Xy ] ; then echo "BYE"; exit 1; \
-                          else echo; fi
+  if [ "$ANSWER" != y ];then echo "BYE"; exit 1; \
+                        else echo; fi
 # ====================================================================== #
-
-  for SRC in `find $SRCDIR -name "*.remote"`
+  for SRC in $ALLSRC
     do
       SRCDIR=`echo $SRC | rev | cut -d "/" -f 2- | rev`
     # ------------------------------------------------- #
-    ( IFS=$'\n'
+     (IFS=$'\n'
       for REMOTE in `cat $SRC               | # USELESS USE OF CAT
                      grep -v "^%"           | # NO LINES STARTING WITH %
                      grep "^[ \t]*https\?:" | # SELECT HTTP(S)
@@ -47,8 +46,8 @@
        do 
           if [ `echo $REMOTE |        # ECHO $REMOTE
                 grep " -*> " |        # SELECT IF '-(--)>'
-                wc -l` -gt 0 ]; then  # COUNT AND CHECK
-                LOKALNAME=`echo $REMOTE     |      # ECHO $REMOTE
+                wc -l` -gt 0 ]        # COUNT AND CHECK
+          then  LOKALNAME=`echo $REMOTE     |      # ECHO $REMOTE
                            sed 's/^.* -*> //'`     # CUT AFTER ->
                   REMOTE=`echo $REMOTE      |      # ECHO $REMOTE
                           sed 's/ -*> .*$//'`      # CUT BEFORE ->
@@ -59,8 +58,8 @@
                            rev                   | # REWIND
                            tr -d '\r'`             # RM CARRIAGE RETURN
               # -- FALLBACK ------------------------------------------- #
-                if [ `echo $LOKALNAME | wc -c` -lt 2 ];then
-                      LOKALNAME=`echo "$REMOTE"  | # ECHO $REMOTE
+                if [ `echo $LOKALNAME | wc -c` -lt 2 ]
+                then  LOKALNAME=`echo "$REMOTE"  | # ECHO $REMOTE
                                  cut -d " " -f 1 | rev | # SELECT/REVERT
                                  cut -d "/" -f 1 | rev`  # SELECT/REVERT
                 fi
@@ -71,11 +70,16 @@
                LOKALNAME=`echo $LOKALNAME  | # ECHO $LOKALNAME
                           sed 's/^[ ]*//'  | # RM LEADING BLANCS
                           sed 's/[ ]*$//'`   # RM TRAILING BLANCS
+               if [ -d $SRCDIR/$LOKALNAME ]
+               then REMOTENAME=`basename $REMOTE`
+                    LOKALNAME=`echo $LOKALNAME/$REMOTENAME | #
+                               tr -s '/'`
+               fi
                   LOKAL="$SRCDIR/$LOKALNAME" # ADD SRC PATH
                LOKALDIR=`echo $LOKAL | rev |      # ECHO $LOKAL/REWIND
                          cut -d "/" -f 2-  | rev` # SELECT/REWIND
-               if [ ! -d $LOKALDIR ];then  # IF NECESSARY ...
-                    mkdir -p $LOKALDIR     # ... CREATE DIRECTORY
+               if [ ! -d $LOKALDIR ]       # IF NECESSARY ...
+               then mkdir -p $LOKALDIR     # ... CREATE DIRECTORY
                fi
 
      # IF REMOTE FILE EXISTS                          #
@@ -83,29 +87,40 @@
        if [ `curl -s -o /dev/null -IL  \
              -w "%{http_code}" "$REMOTE"` == '200' ]
        then 
+            # IF LOKAL FILE EXISTS                           #
+            # ---------------------------------------------- #
+              if [ -f "$LOKAL" ]
+              then 
+                   LTIME=`date -r "$LOKAL" +%Y%m%d%H%M.%S` 
+                   CHECKSUMLOKAL=`md5sum "$LOKAL" | cut -c 1-32`
 
-     # IF LOKAL FILE EXISTS                           #
-     # ---------------------------------------------- #
-       if [ -f "$LOKAL" ]; then
- 
-     # DOWNLOAD IF REMOTE IS NEWER                    #
-     # ---------------------------------------------- #
-       if [ `curl "$REMOTE" -z "$LOKAL" -o "$LOKAL" \
-             -s -L -w %{http_code}` == "200" ]; then
-             echo "Download $LOKAL"
-             curl -RL "$REMOTE" -o "$LOKAL"
-       else  echo "$LOKAL is up-to-date"
-       fi;else
- 
-     # DOWNLOAD IF NO LOKAL FILE                      #
-     # ---------------------------------------------- #
-             curl -RL "$REMOTE" -o "$LOKAL"
+                 # DOWNLOAD IF REMOTE IS NEWER                    #
+                 # ---------------------------------------------- #
+                   if [ `curl "$REMOTE" -z "$LOKAL" -o "$LOKAL" \
+                         -s -L -w %{http_code}` == "200" ]
+                   then  echo "CHECKING:   $LOKAL"
+                         curl -sRL "$REMOTE" -o tmp.tmp
+
+                         if [ -f tmp.tmp ];then
+                              CHECKSUMREMOTE=`md5sum tmp.tmp | #
+                                              cut -c 1-32`     #
+                              if [ "$CHECKSUMLOKAL" != "$CHECKSUMREMOTE" ]
+                              then cp -p tmp.tmp "$LOKAL"
+                              else touch -t "$LTIME" $LOKAL
+                                   echo "UP-TO-DATE: $LOKAL"
+                              fi
+                              rm tmp.tmp
+                         fi
+                   else  echo "UP-TO-DATE: $LOKAL"
+                   fi;else
+
+                 # DOWNLOAD IF NO LOKAL FILE                      #
+                 # ---------------------------------------------- #
+                         curl -RL "$REMOTE" -o "$LOKAL"
+               fi
        fi
-       fi
- 
       done;)
     # ------------------------------------------------- #
-
    done
 # ====================================================================== #
 
